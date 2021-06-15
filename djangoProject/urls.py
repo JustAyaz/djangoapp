dbec12bcbbd26b8d999dfcded248e285db2e9b39
JustAyaz <43115720+JustAyaz@users.noreply.github.com>
2021-06-15 03:19:30 +0300

    v6

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
+from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.static import serve
 
 import news
 from news.views import *
@@ -24,7 +26,11 @@ from news.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('news.urls'), name='home')
+    path('', include('news.urls'), name='home'),
+
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
